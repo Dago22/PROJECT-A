fb72d07513fc88e90f0a9b09da040687c7160d7d
Dago22 <85245738+Dago22@users.noreply.github.com>
2025-11-20 09:47:25 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,18 @@
+
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
 import './index.css'
-createRoot(document.getElementById('root')).render(<BrowserRouter><App/></BrowserRouter>)
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
