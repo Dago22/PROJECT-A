bc549d80efdf4299513c158cc5e60f72d5bd0491
Dago22 <85245738+Dago22@users.noreply.github.com>
2025-11-20 10:07:59 -0800

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'   // <-- fix if App is jsx
+import App from './App.jsx'
 import './index.css'
 
 // Import your Publishable Key
@@ -15,3 +15,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
